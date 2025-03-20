f8003ced0cee5191a5547d16792fe2dc5e688f99
PranadityaTriJyotistaVavitramPS <gedearisudana16@gmail.com>
2025-03-20 15:02:49 +0700

    update router menerima surveyor #1

--- server/routes/surveyorApplyRoutes.js
+++ server/routes/surveyorApplyRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const surveyorApplyController = require('../controllers/surveyorApplyController');
 
-router.post("/mendaftarSurvey", surveyorApplyController.applyToSurvey);
 router.get("/",surveyorApplyController.surveyorWorker)
+router.post("/mendaftarSurvey", surveyorApplyController.applyToSurvey);
+
 
 module.exports = router;
