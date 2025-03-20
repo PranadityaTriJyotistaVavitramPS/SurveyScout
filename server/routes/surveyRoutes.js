7850472591c67649e927026144cbea96cd028703
PranadityaTriJyotistaVavitramPS <gedearisudana16@gmail.com>
2025-03-20 16:55:03 +0700

    update upload jawaban surveyor #2

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -5,7 +5,7 @@ const authenticate = require("../middleware/authenticate")
 
 router.post('/createSurveyDraft',authenticate,surveyController.createSurveyDraft);
 router.post('/createSurveyPayment/:id_draft',surveyController.createSurveyPayment);
-router.post('/uploadAnswer',authenticate,surveyController.uploadAnswers,surveyController.submitSurveyorAnswer)
+router.post('/uploadAnswer',surveyController.uploadAnswers,surveyController.submitSurveyorAnswer)
 router.get('/',surveyController.getAllSurveyTask);
 router.delete('/deleteATask',surveyController.deleteASurvey);
 
