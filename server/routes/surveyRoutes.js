1fbd6e09d1d804f86b18a652946adf9aee5ad2df
PranadityaTriJyotistaVavitramPS <gedearisudana16@gmail.com>
2025-03-23 22:52:50 +0700

    PROJEK SURVEYOR INTEGRASI #15

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -6,13 +6,8 @@ const authenticate = require("../middleware/authenticate")
 
 router.get('/',surveyController.getAllSurveyTask);
 router.get('/mySurvey',authenticate,surveyController.surveyorProjects)
-router.get('/:id_survey', authenticate, async (req, res) => {
-    const { id_survey } = req.params;
-    if (req.user) {
-        return surveyController.getAppliedSurveyDetail(req, res);
-    }
-    return surveyController.getSurveyDetail(req, res); 
-});
+router.get('/:id_survey',surveyController.getSurveyDetail);
+router.get('/:id_survey/applied',authenticate,surveyController.getAppliedSurveyDetail);
 router.post('/createSurveyDraft',authenticate,surveyController.createSurveyDraft);
 router.post('/createSurveyPayment/:id_draft',surveyController.createSurveyPayment);
 router.post('/uploadAnswer',surveyController.uploadAnswers,surveyController.submitSurveyorAnswer)
